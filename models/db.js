6a02245a6298b319a852ecbd8b461e43334d7078
wreran <raeanne.wee@gmail.com>
2026-08-10 03:10:55 +0800

    feat: update db.js to use .env variables

--- models/db.js
+++ models/db.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'das',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT || 3306),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
-    connectionLimit: 10
-})
+    connectionLimit: Number(process.env.DB_CONNECTION_LIMIT || 10)
+});
 
 module.exports = pool;
