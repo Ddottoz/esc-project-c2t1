a7f1af26ece6351f6ebd40a2c4a8522611b9f611
wreran <raeanne.wee@gmail.com>
2026-08-10 03:54:41 +0800

    fix: minor updates

--- models/db.js
+++ models/db.js
@@ -1,17 +1,6 @@
 const mysql = require('mysql2/promise');
 
-// const pool = mysql.createPool({
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-
-//     waitForConnections: true,
-//     connectionLimit: 10,
-// });
-
-// Test database
+// Main railway database
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT || 3306),
