252a34f66d2fc6b3e1791c4733a0acf1b28258df
Dottoz <84174800+Ddottoz@users.noreply.github.com>
2026-08-11 01:46:36 +0800

    Feat: uses real database, added routing

--- models/db.js
+++ models/db.js
@@ -1,27 +1,27 @@
 const mysql = require('mysql2/promise');
 
-// const pool = mysql.createPool({
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-
-//     waitForConnections: true,
-//     connectionLimit: 10,
-// });
-
-// Test database
 const pool = mysql.createPool({
-    host: "localhost",
-    port: 3306,
-    user: "Dottoz",
-    password: "Wmywbyt-123",
-    database: "upload_test",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 
     waitForConnections: true,
-    connectionLimit: 10
+    connectionLimit: 10,
 });
+
+// Test database
+// const pool = mysql.createPool({
+//     host: "localhost",
+//     port: 3306,
+//     user: "Dottoz",
+//     password: "Wmywbyt-123",
+//     database: "upload_test",
+
+//     waitForConnections: true,
+//     connectionLimit: 10
+// });
 
 module.exports = pool;
 
--- models/upload.js
+++ models/upload.js
@@ -1,57 +1,84 @@
 const pool = require("./db");
 
-async function uploadPdf(originalName, storedName, filePath, comment) {
-
+async function createAssessmentSubmission(
+    studentAssessmentId,
+    submittedDate,
+    filePath
+) {
     const sql = `
-        INSERT INTO pdf_files
-        (original_name, stored_name, file_path, comment)
-        VALUES (?, ?, ?, ?)
+        INSERT INTO assessmentSubmission
+        (studentAssessmentId, submittedDate, submittedBy, filepath, isAccepted)
+        VALUES (?, ?, ?, ?, ?)
+        ON DUPLICATE KEY UPDATE
+            submittedDate = VALUES(submittedDate),
+            submittedBy = VALUES(submittedBy),
+            filepath = VALUES(filepath),
+            isAccepted = VALUES(isAccepted)
     `;
 
     const [result] = await pool.query(sql, [
-        originalName,
-        storedName,
+        studentAssessmentId,
+        submittedDate,
+        1,
         filePath,
-        comment
+        0
     ]);
 
     return result;
 }
+// async function uploadPdf(submissionId, originalName, storedName, filePath, comment) {
 
-async function getAllUploads() {
+//     const sql = `
+//         INSERT INTO pdf_files
+//         (original_name, stored_name, file_path, comment)
+//         VALUES (?, ?, ?, ?)
+//     `;
+
+//     const [result] = await pool.query(sql, [
+//         originalName,
+//         storedName,
+//         filePath,
+//         comment
+//     ]);
+
+//     return result;
+// }
+
+async function getAllUploads(studentAssessmentId) {
 
     const [rows] = await pool.query(
         `SELECT 
-            id,
-            original_name AS fileName,
-            file_path AS filePath,
-            comment,
-            analysis
-         FROM pdf_files
-         ORDER BY id DESC`
+            submissionId as id,
+            submittedDate as date,
+            filepath
+         FROM assessmentSubmission
+         WHERE studentAssessmentId = ?
+         ORDER BY submittedDate DESC`,
+         [studentAssessmentId]
     );
 
     return rows;
 }
 
-async function updateAnalysis(id, analysis) {
+// async function updateAnalysis(id, analysis) {
 
-    const sql = `
-        UPDATE pdf_files
-        SET analysis = ?
-        WHERE id = ?
-    `;
+//     const sql = `
+//         UPDATE pdf_files
+//         SET analysis = ?
+//         WHERE id = ?
+//     `;
 
-    const [result] = await pool.query(sql, [
-        JSON.stringify(analysis),
-        id
-    ]);
+//     const [result] = await pool.query(sql, [
+//         JSON.stringify(analysis),
+//         id
+//     ]);
 
-    return result;
-}
+//     return result;
+// }
 
 module.exports = {
-    uploadPdf,
-    updateAnalysis,
+    createAssessmentSubmission,
+    //uploadPdf,
+    //updateAnalysis,
     getAllUploads
 };
